a531622d9ac76eb124a67ed7ef8c6fb78b9ebf0a
vagnes <waagnes@outlook.com>
2019-06-07 17:27:11 +0200

    Refactored print formatting, added more formatting options

--- server/routes.py
+++ server/routes.py
@@ -22,19 +22,41 @@ class LFCR(Resource):
     def get(self):
         printer.feed(1)
 
-class Bold(Resource):
+class Format(Resource):
 
     def post(self):
         args = request.get_json()
+
+        print(args)
+
+        #bold
         if args["bold"] is True:
             printer.boldOn()
         else:
             printer.boldOff()
 
+        #center
+        if args["center"] is True:
+            printer.justify("C")
+        else:
+            printer.justify("L")
+
+        #double height
+        if args["height2x"] is True:
+            printer.doubleHeightOn()
+        else:
+            printer.doubleHeightOff()
+        
+        #double width
+        if args["underline"] is True:
+            printer.underlineOn()
+        else:
+            printer.underlineOff()
+
+
 api.add_resource(Test, "/test/")
 api.add_resource(PrintText, "/print_text/")
-api.add_resource(LFCR, "/lfcr/")
-api.add_resource(Bold, "/bold/")
+api.add_resource(Format, "/format/")
 
 if __name__ == "__main__":
     app.run(port=5001, debug=True)
